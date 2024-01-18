4a2ee09e8e095ed9c7b6b7d766e4c3d389af3c64
Tiro C <tchoabi@gmail.com>
2024-01-18 20:24:38 +0200

    clean up using setup in the script tag

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,10 @@
     <TransactionList />
     <AddTransaction />
 </template>
-<script>
+<script setup>
   import Header from './components/Header.vue';
   import Balance from './components/Balance.vue';
   import IncomeExpenses from './components/IncomeExpenses.vue';
   import TransactionList from './components/TransactionList.vue';
   import AddTransaction from './components/AddTransaction.vue';
-
-  export default{
-    components:{
-      Header,
-      Balance,
-      IncomeExpenses,
-      TransactionList,
-      AddTransaction,
-    },
-  };
 </script>
