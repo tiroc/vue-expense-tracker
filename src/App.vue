00e3002da1c5921b3760da2910fb9c742e713059
Tiro C <tchoabi@gmail.com>
2024-01-18 16:21:19 +0200

    import transactionList into App

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,20 @@
   <div class="container"></div>
     <Balance />
     <IncomeExpenses />
+    <TransactionList />
 </template>
 <script>
   import Header from './components/Header.vue';
   import Balance from './components/Balance.vue';
   import IncomeExpenses from './components/IncomeExpenses.vue';
+  import TransactionList from './components/TransactionList.vue';
 
   export default{
     components:{
       Header,
       Balance,
       IncomeExpenses,
+      TransactionList,
     },
   };
 </script>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,15 @@
+<template>
+    <h3>History</h3>
+    <ul id="list" class="list">
+        <li class="minus">
+            Cash
+            <span>-R400</span>
+            <button class="delete-btn">x</button>
+        </li>
+        <li class="plus">
+            Paycheck
+            <span>+R50</span>
+            <button class="delete-btn">x</button>
+        </li>
+    </ul>
+</template>
