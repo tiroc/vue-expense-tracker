04061f91008093bb5c4f492399b1fc403f9a82df
Tiro C <tchoabi@gmail.com>
2024-01-18 15:32:26 +0200

    show header template in app

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,12 @@
-<template>My App</template>
+<template>
+  <Header />
+</template>
+<script>
+  import Header from './components/Header.vue';
+
+  export default{
+    components:{
+      Header,
+    },
+  };
+</script>
