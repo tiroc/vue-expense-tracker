bb886a5dab04c6ece545a7b1e66ae8605f6b43f0
Tiro C <tchoabi@gmail.com>
2024-01-18 18:56:19 +0200

    how to display data using the options API

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,15 +1,25 @@
 <template>
     <h3>History</h3>
     <ul id="list" class="list">
-        <li class="minus">
-            Cash
-            <span>-R400</span>
-            <button class="delete-btn">x</button>
-        </li>
-        <li class="plus">
-            Paycheck
-            <span>+R500</span>
+        
+        <li v-for="transaction in transactions" v-bind:key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
+            {{ transaction.text }}
+            <span>R{{ transaction.amount }}</span>
             <button class="delete-btn">x</button>
         </li>
     </ul>
 </template>
+
+<script>
+    export default{
+        data(){
+            return {
+                transactions: [
+                    {id: 1, text: 'Flower', amount: -20},
+                    {id: 2, text: 'Takeout', amount: -99},
+                    {id: 3, text: 'Old PC', amount: +900},
+                ],
+            };
+        }
+    }
+</script>
