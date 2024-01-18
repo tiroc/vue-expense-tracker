c09a51dea440a5476265785d2400085c16dceabe
Tiro C <tchoabi@gmail.com>
2024-01-18 16:02:26 +0200

    import Balance component into app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <Header />
+  <div class="container"></div>
+    <Balance />
 </template>
 <script>
   import Header from './components/Header.vue';
+  import Balance from './components/Balance.vue';
 
   export default{
     components:{
       Header,
+      Balance,
     },
   };
 </script>
